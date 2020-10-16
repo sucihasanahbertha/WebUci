c797f54f4a2e7a2c083f8c7b3e0716733c7ce233
Uci <ucibertha23@gmail.com>
2020-10-16 18:07:06 +0700

    my commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,5 +8,12 @@ class Home extends BaseController
 	}
 
 	//--------------------------------------------------------------------
-
+	public function show()
+	{
+		$data['nama'] = 'Uciber';
+		$data['npm'] = '1817051003';
+		echo view('mahasiswa/header', $data);
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer', $data);
+	}
 }
---
+++ app/Views/mahasiswa/index.php
@@ -0,0 +1,4 @@
+<h3>Biodata</h3>
+<h3>Nama:<?php echo $nama ?></h3>
+<h3>NPM:<?php echo $npm ?></h3>
+
